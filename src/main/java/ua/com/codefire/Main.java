7dcc7d0d2d0d16bd7d7c3cb91fe7242fe4716cd7
Alex Melnyk <melnyk@codefire.com.ua>
2017-01-30 20:04:10 +0200

    Project files added.

---
+++ src/main/java/ua/com/codefire/Main.java
@@ -0,0 +1,10 @@
+package ua.com.codefire;
+
+/**
+ * Created by human on 1/30/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello GIT!");
+    }
+}
